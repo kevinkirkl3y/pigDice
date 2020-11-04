d86b5ae060725f450a3a496d11d7538cdefaa093
kevinkirkl3y <kevinmkirkley@gmail.com>
2020-11-04 10:45:14 -0800

    successfully tested the creation of Gamer Object
    Co-authored-by: Nataliya Zhuravleva <natalindria@gmail.com>
    Co-authored-by: Tawnee Harris <tawneeh@icloud.com>

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+
+function Gamer(gamerName, currentScore, totalScore) {
+  this.gamerName = gamerName;
+  this.currentScore = currentScore;
+  this.totalScore = totalScore;
+};
+let gamer1=new Gamer("Kevin", 0,0);
+let gamer2=new Gamer("Vlad", 0,0);
